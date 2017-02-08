2b2dbd3f7cfa86836e9d7abf56deec2d68411bb6
John <jvohnny@gmail.com>
2017-02-09 01:56:58 +0700

    implemented psi concentration graph

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -5,42 +5,66 @@ angular.module('JohnToDoApp2', ['TodoService', 'chart.js'])
     // console.log('helloooooooo')
     // $scope.todos = [];
     // $scope.input = [];
-    $scope.apap = null;
-    $scope.bdrawn = null;
-    $scope.tnac = null;
-    $scope.ti = null;
-    $scope.final_psi = 0;
+    vm = $scope;
 
-    $scope.labelsCT = ["A", "B", "D", "E", "F", "G", "H"];
-    $scope.seriesCT = ['Series A'];
-    $scope.dataCT = [
-        [90, 75, 65, 53, 45, 35, 30]
+    vm.apap = null;
+    vm.bdrawn = null;
+    vm.tnac = null;
+    vm.ti = null;
+    vm.final_psi = 0;
+    vm.apap_th = 45.0;
+    vm.k3 = (Math.log(2))/4.0;
+
+    vm.labelsCT = ["A", "B", "D", "E", "F", "G", "H"];
+    vm.seriesCT = ['Series A'];
+    vm.dataCT = [
+        [90, 75, 65, 53, 45, 35, 30],
+        []
     ];
+    vm.dataCT_ti = [4];
 
-    $scope.labelsRisk = [1, 2, 3, 4, 5, 6, 7];
-    $scope.seriesRisk = ['Series B'];
-    $scope.dataRisk = [
+
+    vm.labelsRisk = [1, 2, 3, 4, 5, 6, 7];
+    vm.seriesRisk = ['Series B'];
+    vm.dataRisk = [
         [100, 50, 25, 12.5, 6.25, 3, 1.5]
     ];
 
-    $scope.getPSI = function(apap,bdrawn,tnac,ti) {
-      var apap_th = 45.0;
+    vm.getPSI = function(apap,bdrawn,tnac,ti) {
       var apap_lvl = apap*(Math.pow(2,((bdrawn-4)/4.0)))
-      var k3 = (Math.log(2))/4.0;
+      // var k3 = (Math.log(2))/4.0;
       var double_apap = apap_lvl * 2.0;
-      var tf = Math.log(double_apap/apap_th)/k3;
+      var tf = Math.log(double_apap/vm.apap_th)/vm.k3;
 
       var tff = (tnac - tf < 0 ? tnac : tf);
 
-      var e_kts = Math.exp((-k3*ti)) - Math.exp((-k3*tff));
-      var psi_left = (1.0/k3) * double_apap * e_kts;
-      var psi_right = (tff - ti)*apap_th
+      var e_kts = Math.exp((-vm.k3*ti)) - Math.exp((-vm.k3*tff));
+      var psi_left = (1.0/vm.k3) * double_apap * e_kts;
+      var psi_right = (tff - ti)*vm.apap_th
       var psi = psi_left - psi_right
 
-      $scope.final_psi = psi;
+      vm.final_psi = psi;
     }
 
+    // return apap_lvl * np.exp(-k3*(t-tnac))
+    vm.graphPSI = function(apap,bdrawn,tnac,ti){
+      var apap_lvl = apap*(Math.pow(2,((bdrawn-4)/4.0)))
 
+      for(var hour=[],b=24;b>0;hour[--b]=b+1);
+      for(var threshold=[],b=hour.length;b>0;threshold[--b]=vm.apap_th);
+      // console.log(threshold);
+
+      var psi_val = function(t){
+        return apap_lvl * Math.exp(-vm.k3*(t-tnac))/4.0;
+      }
+
+      vm.labelsCT = hour;
+      vm.dataCT[0] = hour.map(psi_val);
+
+      vm.dataCT[1] = threshold;
+      console.log(vm.dataCT);
+
+    }
 
     // function getAllTodos() {
     //   Todo.getTodos()
